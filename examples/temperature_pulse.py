ff816319538b3391926356725fb6cdb5a08f5120
Salvatore La Bua <slabua@gmail.com>
2021-02-14 21:13:35 +0900

    Major refactoring of temperature_pulse.py example
    Added some parametric variables.
    Added functions for each basic task.

--- examples/temperature_pulse.py
+++ examples/temperature_pulse.py
@@ -1,30 +1,55 @@
 from machine import Pin, PWM
 from time import sleep
 
+
 sensor_temp = machine.ADC(4)
-conversion_factor = 3.3 / (65535)
 
 pwm = machine.PWM(machine.Pin(25))
 pwm.freq(1000)
 
-while True:
-    reading = sensor_temp.read_u16() * conversion_factor
-    
-    temperature = 27 - (reading - 0.706)/0.001721
-    print(temperature)
-    
-    if temperature < 19:
-        steps = 16
-    elif temperature > 23:
-        steps = 1024
+conversion_factor = 3.3 / (65535)
+
+threshold_min = 19
+threshold_max = 23
+
+steps_slow = 16
+steps_fast = 1024
+
+brightness_low = 0
+brightness_high = 65025
+
+
+def temperature(reading):
+    return 27 - (reading - 0.706)/0.001721
+
+
+def steps(temp, th_min, th_max, steps_slow, steps_fast):
+    if temp < th_min:
+        n_steps = steps_slow
+    elif temp > th_max:
+        n_steps = steps_fast
     else:
-        steps = temperature
+        n_steps = temp
     
-    for duty in range(0, 65025, int(steps**2)):
+    return n_steps
+
+
+def pulse(n_steps, br_high, br_low):
+    for duty in range(br_low, br_high, int(n_steps**2)):
         pwm.duty_u16(duty)
         sleep(0.001)
-    for duty in range(65025, 0, -int(steps**3)):
+    for duty in range(br_high, br_low, -int(n_steps**3)):
         pwm.duty_u16(duty)
         sleep(0.05)
 
-        
+
+while True:
+    reading = sensor_temp.read_u16() * conversion_factor
+
+    temp = temperature(reading)
+    print(temp)
+    
+    n_steps = steps(temp, threshold_min, threshold_max, steps_slow, steps_fast)
+    
+    pulse(n_steps, brightness_high, brightness_low)
+
