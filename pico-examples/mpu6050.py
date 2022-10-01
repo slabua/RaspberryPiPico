2df68b7240768fc8dd9364d9640644439ae0fcb2
Salvatore La Bua <slabua@gmail.com>
2022-10-02 03:53:50 +0900

    Update MPU example

--- pico-examples/mpu6050.py
+++ pico-examples/mpu6050.py
@@ -1,27 +1,31 @@
 from imu import MPU6050
-import time
 from machine import Pin, I2C
+from time import sleep
 
 i2c = I2C(0, sda=Pin(0), scl=Pin(1), freq=400000)
+
 print(i2c.scan())
 imu = MPU6050(i2c)
+imu.accel_range = 0
+imu.gyro_range = 0
+# imu.filter_range = 0
+
+period = 0.01
+
+print(imu.accel_range, imu.gyro_range)
 
 while True:
-    #print(imu.accel.xyz, imu.gyro.xyz, imu.temperature, end='\r')
-    ax = round(imu.accel.x, 2)
-    ay = round(imu.accel.y, 2)
-    az = round(imu.accel.z, 2)
-    gx = round(imu.gyro.x)
-    gy = round(imu.gyro.y)
-    gz = round(imu.gyro.z)
-    temp = round(imu.temperature, 2)
-    print(
-        ax, "\t",
-        ay, "\t",
-        az, "\t",
-        gx, "\t",
-        gy, "\t",
-        gz, "\t",
-        temp, "        ",
-        end="\r"
-    )
+    # print(imu.accel.xyz,imu.gyro.xyz,imu.temperature,end='\r')
+
+    # Read sensor data
+    aX = imu.accel.x
+    aY = imu.accel.y
+    aZ = imu.accel.z
+    gX = imu.gyro.x
+    gY = imu.gyro.y
+    gZ = imu.gyro.z
+    tem = imu.temperature
+    print(aX, "\t", aY, "\t", aZ, "\t", gX, "\t", gY,
+          "\t", gZ, "\t", tem, "        ", end="\r")
+
+    sleep(period)
