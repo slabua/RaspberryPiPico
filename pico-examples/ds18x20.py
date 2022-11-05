5d9ebbc598b9e41bfc2b5b167e081048984cbb46
Salvatore La Bua <slabua@gmail.com>
2022-11-05 23:50:04 +0900

    Linting for ds18x20 example

--- pico-examples/ds18x20.py
+++ pico-examples/ds18x20.py
@@ -1,5 +1,8 @@
-import machine, onewire, ds18x20, time
- 
+import machine
+import onewire
+import ds18x20
+import time
+
 ds_pin = machine.Pin(11)
 ds_sensor = ds18x20.DS18X20(onewire.OneWire(ds_pin))
 
@@ -14,4 +17,4 @@ while True:
     time.sleep_ms(750)
     for i in range(len(roms)):
         print("T" + str(i) + ": " + str(ds_sensor.read_temp(roms[i])))
-    #time.sleep(0.15)
+    # time.sleep(0.15)
