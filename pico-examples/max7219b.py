ddf7e797b3b28eab613bd076381d9c940a97bbe4
Salvatore La Bua <slabua@gmail.com>
2023-10-09 01:00:43 +0900

    Update MAX7219 example

--- pico-examples/max7219b.py
+++ pico-examples/max7219b.py
@@ -1,11 +1,12 @@
 from machine import Pin, SPI
+# import math
 import max7219
-import time
+from time import sleep
 
 spi = SPI(0, baudrate=8000000, polarity=1, phase=0, sck=Pin(2), mosi=Pin(3))
 cs = Pin(5, Pin.OUT)
 
-n = 4
+n = 8
 display = max7219.Matrix8x8(spi, cs, n)
 display.brightness(1)
 
@@ -25,11 +26,15 @@ GLYPHS = {
     ],
 }
 
+length = len(text1) + 1 + len(text2)
+# repeat = math.ceil(n / length)
+
 display.fill(0)
 display.show()
 
 while True:
-    for x in range(n * 8, -(len(text1) + 1 + len(text2)) * 8, -1):
+    # for x in range(n * 8, -(length * 8) - (n * 8), -1):
+    for x in range(n * 8, -(length * 8), -1):
         display.fill(0)
 
         display.text(text1, x, 1, 1)
@@ -37,4 +42,4 @@ while True:
         display.text(text2, x + 9 + (len(text1) * 8), 1, 1)
         display.show()
 
-        time.sleep(0.025)
+        sleep(0.025)
