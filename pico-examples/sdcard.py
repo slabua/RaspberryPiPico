613b3978d4cdaf7df7e551c3f0a8329cc65e2b70
Salvatore La Bua <slabua@gmail.com>
2024-07-16 15:13:17 +0900

    Add sdcard read/write example

---
+++ pico-examples/sdcard.py
@@ -0,0 +1,24 @@
+from machine import Pin, SPI
+import os
+import sdcard
+import time
+
+cs_pin = Pin(28, Pin.OUT)
+
+spi = SPI(
+    id=1,
+    sck=Pin(10, Pin.OUT),
+    mosi=Pin(11, Pin.OUT),
+    miso=Pin(8, Pin.OUT),
+)
+
+sd = sdcard.SDCard(spi, cs_pin)
+os.mount(sd, '/sd')
+os.chdir('sd')
+os.listdir()
+
+file = open('/sd/test.txt', 'w')
+file.write(str(time.localtime()))
+file.close()
+
+print(open('test.txt').read())
